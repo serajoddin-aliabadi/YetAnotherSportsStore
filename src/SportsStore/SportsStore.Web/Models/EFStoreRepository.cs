b7a1d6672a6029bf05fbab53a6699f4bcb26b6e7
Serajoddin Aliabadi PC <40219085+serajoddin-aliabadi@users.noreply.github.com>
2023-06-20 18:29:12 +0330

    Add database and model

---
+++ src/SportsStore/SportsStore.Web/Models/EFStoreRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace SportsStore.Web.Models;
+
+public class EFStoreRepository : IStoreRepository
+{
+	private StoreDbContext context;
+
+	public EFStoreRepository(StoreDbContext ctx)
+	{
+		context = ctx;
+	}
+
+	public IQueryable<Product> Products => context.Products;
+}
--- src/SportsStore/SportsStore.Web/Program.cs
+++ src/SportsStore/SportsStore.Web/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using SportsStore.Web.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<StoreDbContext>(opt =>
+{
+	opt.UseSqlServer(builder.Configuration.GetConnectionString("SportsStoreConnection"));
+});
+builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();
 
 
 var app = builder.Build();
@@ -9,6 +17,9 @@ var app = builder.Build();
 
 app.UseStaticFiles();
 app.MapDefaultControllerRoute();
+
+
+SeedData.EnsurePopulated(app);
 
 
 app.Run();
