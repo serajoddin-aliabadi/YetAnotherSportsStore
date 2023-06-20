86c070246057f1a91f2ee7a923b37215abebb62f
Serajoddin Aliabadi PC <40219085+serajoddin-aliabadi@users.noreply.github.com>
2023-06-20 19:09:56 +0330

    Show a simple list of Products

--- src/SportsStore/SportsStore.Web/Controllers/HomeController.cs
+++ src/SportsStore/SportsStore.Web/Controllers/HomeController.cs
@@ -1,10 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Web.Models;
 
 namespace SportsStore.Web.Controllers;
 
 public class HomeController : Controller
 {
+	private IStoreRepository repository;
+
+	public HomeController(IStoreRepository repo)
+	{
+		repository = repo;
+	}
+
 
 	public IActionResult Index()
-		=> View();
+		=> View(repository.Products);
 }
