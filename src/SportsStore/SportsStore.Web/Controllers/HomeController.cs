d6009810cbfc4e28a0f9e60d58047bf7441127c0
Serajoddin Aliabadi PC <40219085+serajoddin-aliabadi@users.noreply.github.com>
2023-06-22 14:46:43 +0330

    Enable filtering by category in Home controller.

--- src/SportsStore/SportsStore.Web/Controllers/HomeController.cs
+++ src/SportsStore/SportsStore.Web/Controllers/HomeController.cs
@@ -16,20 +16,22 @@ public class HomeController : Controller
 	public int PageSize = 4;
 
 
-	public ViewResult Index(int productPage = 1)
+	public ViewResult Index(string? category, int productPage = 1)
 	{
 		return View(new ProductsListViewModel
 		{
 			Products = repository.Products
-					   .OrderBy(p => p.ProductID)
-					   .Skip((productPage - 1) * PageSize)
-					   .Take(PageSize),
+				.Where(p => category == null || p.Category == category)
+				.OrderBy(p => p.ProductID)
+				.Skip((productPage - 1) * PageSize)
+				.Take(PageSize),
 			PagingInfo = new PagingInfo
 			{
 				CurrentPage = productPage,
 				ItemsPerPage = PageSize,
 				TotalItems = repository.Products.Count()
-			}
+			},
+			CurrentCategory = category
 		});
 	}
 }
