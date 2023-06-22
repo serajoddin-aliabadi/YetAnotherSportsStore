bf032a417bfd62ca27727107ed65e1daa66596e1
Serajoddin Aliabadi PC <40219085+serajoddin-aliabadi@users.noreply.github.com>
2023-06-22 16:48:49 +0330

    Implement cart page.

--- src/SportsStore/SportsStore.Web/Pages/Cart.cshtml.cs
+++ src/SportsStore/SportsStore.Web/Pages/Cart.cshtml.cs
@@ -1,12 +1,40 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SportsStore.Web.Infrastructure;
+using SportsStore.Web.Models;
 
-namespace SportsStore.Web.Pages
+namespace SportsStore.Web.Pages;
+
+public class CartModel : PageModel
 {
-    public class CartModel : PageModel
-    {
-        public void OnGet()
-        {
-        }
-    }
+	private IStoreRepository repository;
+
+	public CartModel(IStoreRepository repo)
+	{
+		repository = repo;
+	}
+
+	public Cart? Cart { get; set; }
+	public string ReturnUrl { get; set; } = "/";
+
+	public void OnGet(string returnUrl)
+	{
+		ReturnUrl = returnUrl ?? "/";
+		Cart = HttpContext.Session.GetJson<Cart>("cart")
+			   ?? new Cart();
+	}
+
+	public IActionResult OnPost(long productId, string returnUrl)
+	{
+		Product? product = repository.Products
+			.FirstOrDefault(p => p.ProductID == productId);
+		if (product != null)
+		{
+			Cart = HttpContext.Session.GetJson<Cart>("cart")
+				   ?? new Cart();
+			Cart.AddItem(product, 1);
+			HttpContext.Session.SetJson("cart", Cart);
+		}
+		return RedirectToPage(new { returnUrl = returnUrl });
+	}
 }
