8076805c786a27f22cf03be78ebd52a84d61ef56
Serajoddin Aliabadi PC <40219085+serajoddin-aliabadi@users.noreply.github.com>
2023-06-22 15:08:56 +0330

    Apply category filtering in routing.

--- src/SportsStore/SportsStore.Web/Program.cs
+++ src/SportsStore/SportsStore.Web/Program.cs
@@ -17,9 +17,20 @@ var app = builder.Build();
 
 app.UseStaticFiles();
 
+app.MapControllerRoute("catpage",
+	"{category}/Page{productPage:int}",
+	new { Controller = "Home", action = "Index" });
+
+app.MapControllerRoute("page", "Page{productPage:int}",
+	new { Controller = "Home", action = "Index", productPage = 1 });
+
+app.MapControllerRoute("category", "{category}",
+	new { Controller = "Home", action = "Index", productPage = 1 });
+
 app.MapControllerRoute("pagination",
 	"Products/Page{productPage}",
-	new { Controller = "Home", action = "Index" });
+	new { Controller = "Home", action = "Index", productPage = 1 });
+
 app.MapDefaultControllerRoute();
 
 
