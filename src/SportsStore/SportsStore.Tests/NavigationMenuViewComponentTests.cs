b709d81cf49908a8f3a67ba843753c311f4c39f3
Serajoddin Aliabadi PC <40219085+serajoddin-aliabadi@users.noreply.github.com>
2023-06-22 15:20:30 +0330

    Add view component to showing categories as Navigation menu

---
+++ src/SportsStore/SportsStore.Tests/NavigationMenuViewComponentTests.cs
@@ -0,0 +1,35 @@
+﻿using SportsStore.Web.Components;
+
+namespace SportsStore.Tests;
+
+public class NavigationMenuViewComponentTests
+{
+	[Fact]
+	public void Can_Select_Categories()
+	{
+		// Arrange
+		Mock<IStoreRepository> mock = new Mock<IStoreRepository>();
+		mock.Setup(m => m.Products).Returns((new Product[] {
+			new Product {ProductID = 1, Name = "P1",
+				Category = "Apples"},
+			new Product {ProductID = 2, Name = "P2",
+				Category = "Apples"},
+			new Product {ProductID = 3, Name = "P3",
+				Category = "Plums"},
+			new Product {ProductID = 4, Name = "P4",
+				Category = "Oranges"},
+		}).AsQueryable<Product>());
+
+		NavigationMenuViewComponent target =
+			new NavigationMenuViewComponent(mock.Object);
+
+		// Act = get the set of categories
+		string[] results = ((IEnumerable<string>?)(target.Invoke()
+								as ViewViewComponentResult)?.ViewData?.Model
+							?? Enumerable.Empty<string>()).ToArray();
+
+		// Assert
+		Assert.True(Enumerable.SequenceEqual(new string[] { "Apples",
+			"Oranges", "Plums" }, results));
+	}
+}
---
+++ src/SportsStore/SportsStore.Web/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,22 @@
+﻿namespace SportsStore.Web.Components;
+
+using Microsoft.AspNetCore.Mvc;
+using Models;
+
+public class NavigationMenuViewComponent : ViewComponent
+{
+	private IStoreRepository repository;
+
+	public NavigationMenuViewComponent(IStoreRepository repo)
+	{
+		repository = repo;
+	}
+
+	public IViewComponentResult Invoke()
+	{
+		return View(repository.Products
+			.Select(x => x.Category)
+			.Distinct()
+			.OrderBy(x => x));
+	}
+}
