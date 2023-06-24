3b374c62dbf8e09c5ca21a5124efb82e2874009e
Serajoddin Aliabadi PC <40219085+serajoddin-aliabadi@users.noreply.github.com>
2023-06-24 15:00:38 +0330

    Show cart at layout with view component

---
+++ src/SportsStore/SportsStore.Web/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Web.Models;
+
+namespace SportsStore.Web.Components;
+
+public class CartSummaryViewComponent : ViewComponent
+{
+	private Cart cart;
+
+	public CartSummaryViewComponent(Cart cartService)
+	{
+		cart = cartService;
+	}
+
+	public IViewComponentResult Invoke()
+	{
+		return View(cart);
+	}
+}
