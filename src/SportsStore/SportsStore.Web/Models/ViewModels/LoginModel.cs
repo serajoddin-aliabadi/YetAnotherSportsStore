75d305a2b6c3e67a8462f9ae9f98b5ec830090a0
Serajoddin Aliabadi PC <40219085+serajoddin-aliabadi@users.noreply.github.com>
2023-06-26 12:40:04 +0330

    Implement Login and Authorization

---
+++ src/SportsStore/SportsStore.Web/Models/ViewModels/LoginModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SportsStore.Web.Models.ViewModels;
+
+public class LoginModel
+{
+	public required string Name { get; set; }
+
+	public required string Password { get; set; }
+
+	public string ReturnUrl { get; set; } = "/";
+}
